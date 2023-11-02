f5c73d46445aef8fcac7de507174f7e6657ea42a
Роман Литвиненко <romantolstyaklitvinenko@gmail.com>
2023-11-03 01:35:27 +0600

    Text fixed + added demo, install doesn't work yet

--- app/src/main/java/com/example/android_development_dota2/MainActivity.kt
+++ app/src/main/java/com/example/android_development_dota2/MainActivity.kt
@@ -3,27 +3,26 @@ package com.example.android_development_dota2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.material3.Text
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.zIndex
 import com.example.android_development_dota2.ui.theme.Android_development_dota2Theme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,10 +33,11 @@ class MainActivity : ComponentActivity() {
                     color = Color.Black,
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    ImageWithLogo(
-                        painterResource(id = R.drawable.main_photo_dota),
-                        painterResource(id = R.drawable.logo)
-                    )
+                    Column(modifier = Modifier.fillMaxWidth()) {
+                        ImageWithLogo()
+                        DisplayText()
+                        DotaDemo()
+                    }
                 }
             }
         }
@@ -45,45 +45,110 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ImageWithLogo(image: Painter, logo: Painter) {
-    Box(modifier = Modifier.fillMaxSize()) {
+fun ImageWithLogo() {
+    Box(modifier = Modifier.fillMaxWidth()) {
         Image(
-            painter = image,
-            contentDescription = null,
+            painter = painterResource(id = R.drawable.main_photo_dota),
+            contentDescription = "header_photo",
             modifier = Modifier
                 .fillMaxWidth()
-                .height(270.dp),
+                .height(300.dp),
             contentScale = ContentScale.Crop
         )
-        Image(
-            painter = logo,
-            contentDescription = null,
+        Box(
             modifier = Modifier
-                .width(105.dp)
-                .height(95.dp)
-                .offset(x = 10.dp, y = 230.dp), // Установка смещения для логотипа
-        )
-        DisplayText()
+                .padding(15.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "logo",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .width(105.dp)
+                    .height(95.dp)
+                    .offset(x = 10.dp, y = 230.dp), // Установка смещения для логотипа
+            )
+        }
     }
 }
+
 
 @Composable
 fun DisplayText() {
     Text(
-        text = "Dota 2 is a multiplayer online battle arena (MOBA) game which has two teams of five players compete to collectively destroy a large structure defended by the opposing team known as the \"Ancient\", whilst defending their own.",
-        textAlign = TextAlign.Center,
-        fontSize = 16.sp,
-        color = Color.White
+        modifier = Modifier.padding(24.dp),
+        color = Color.White,
+        text = "Dota 2 is a multiplayer online battle arena (MOBA) game which has two teams of five players compete to collectively destroy a large structure defended by the opposing team known as the \"Ancient\", whilst defending their own."
     )
 }
 
+@Composable
+fun DotaDemo() {
+    LazyRow {
+        item {
+            Image(
+                painter = painterResource(id = R.drawable.demo_photo1),
+                contentDescription = "first_demo_photo",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .background(Color.Transparent)
+                    .height(120.dp)
+                    .padding(horizontal = 24.dp)
+            )
+        }
+        item {
+            Image(
+                painter = painterResource(id = R.drawable.demo_photo1),
+                contentDescription = "second_demo_photo",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .background(Color.Transparent)
+                    .height(120.dp)
+                    .padding(horizontal = 24.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun Install() {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        DotaDemo()
+        Spacer(modifier = Modifier.height(16.dp))
+        Box(
+            modifier = Modifier
+                .size(150.dp)
+                .background(Color.Gray)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.install),
+                contentDescription = "Install Image",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+        }
+    }
+}
+
+
+
 @Preview(showBackground = true)
 @Composable
-fun ImageWithLogoPreview() {
+fun ScreenPreview() {
     Android_development_dota2Theme {
-        ImageWithLogo(
-            painterResource(id = R.drawable.main_photo_dota),
-            painterResource(id = R.drawable.logo)
-        )
+        Surface(
+            color = Color.Black,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Column(modifier = Modifier.fillMaxSize()) {
+                ImageWithLogo()
+                DisplayText()
+                DotaDemo()
+                Install()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/android_development_dota2/ui/theme/fonts.kt
@@ -0,0 +1,13 @@
+package com.example.android_development_dota2.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+/*
+import android_development_dota2.R
+
+internal val FontFamily = FontFamily(
+    Font(R.font.sk_modernist_regular_font),
+    Font(R.font.sk_modernist_medium_font, weight = FontWeight.Medium),
+    Font(R.font.sk_modernist_bold_font, weight = FontWeight.Bold)
+)*/
