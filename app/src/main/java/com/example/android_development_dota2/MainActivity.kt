fa22b70e6a430a3ff371716501fd2a3775aea193
Роман Литвиненко <romantolstyaklitvinenko@gmail.com>
2023-11-09 17:03:53 +0600

    Edge to edge added

--- app/src/main/java/com/example/android_development_dota2/MainActivity.kt
+++ app/src/main/java/com/example/android_development_dota2/MainActivity.kt
@@ -16,16 +16,21 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
 import com.example.android_development_dota2.ui.theme.Android_development_dota2Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             Android_development_dota2Theme {
+                ApplySystemBarColors()
                 Surface(
                     color = Color.Black,
                     modifier = Modifier.fillMaxSize()
@@ -67,5 +72,15 @@ fun ScreenPreview() {
                 InstallButton()
             }
         }
+    }
+}
+
+@Composable
+private fun ApplySystemBarColors() {
+    val systemUiController = rememberSystemUiController()
+
+    SideEffect {
+        systemUiController.setStatusBarColor(color = Color.Transparent)
+        systemUiController.setNavigationBarColor(color = Color.Transparent)
     }
 }
